d832df848a4624bea8d16c479a2deacf75190227
arthur.nunes <arthur.nunes@dtidigital.com.br>
2021-05-19 13:55:13 -0300

    custom categorys drawer

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtualflutter/tiles/drawer_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
+
+  final PageController pageController;
+  CustomDrawer(this.pageController);
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +13,7 @@ class CustomDrawer extends StatelessWidget {
           decoration: BoxDecoration(
               gradient: LinearGradient(
                   colors: [
-                    Color.fromARGB(255, 33, 156, 168),
+                    Color.fromARGB(255, 156, 201, 205),
                     Color.fromARGB(255, 170, 238, 222),
                   ],
                   begin: Alignment.topCenter,
@@ -37,10 +41,42 @@ class CustomDrawer extends StatelessWidget {
                        child: Text("Lolja",
                        style: TextStyle(fontSize: 34.0, fontWeight: FontWeight.bold)
                        ),
+                     ),
+                     Positioned(
+                       left: 0.0,
+                       bottom: 0.0,
+                       child: Column(
+                         crossAxisAlignment: CrossAxisAlignment.start,
+                         children: <Widget>[
+                           Text("Olá,",
+                           style: TextStyle(
+                             fontSize: 18.0,
+                             fontWeight: FontWeight.bold
+                           ),
+                           ),
+                           GestureDetector(
+                             child: Text("Entre ou Cadastre-se",
+                               style: TextStyle(
+                                   color: Theme.of(context).primaryColor,
+                                   fontSize: 16.0,
+                                   fontWeight: FontWeight.bold
+                               ),
+                            ),
+                             onTap: (){
+
+                             },
+                           )
+                         ],
+                       ),
                      )
                    ],
                  ),
-               )
+               ),
+               Divider(),
+               DrawerTile(Icons.home, "Inicio", pageController,0),
+               DrawerTile(Icons.list, "Produtos", pageController, 1),
+               DrawerTile(Icons.location_on, "Lojas", pageController, 2),
+               DrawerTile(Icons.playlist_add_check, "Meus Pedidos", pageController, 3),
              ],
           )
         ],
