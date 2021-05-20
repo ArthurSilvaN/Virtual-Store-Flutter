950c85806a1d31ff5aae07ec83cfeb4a76ee2c77
arthur.nunes <arthur.nunes@dtidigital.com.br>
2021-05-20 11:58:58 -0300

    added ScopedModel to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:loja_virtualflutter/models/user_model.dart';
 import 'package:loja_virtualflutter/screens/home_screen.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,14 +14,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Flutter's Clothing",
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        primaryColor: Color.fromARGB(255, 4, 125, 141)
+    return ScopedModel<UserModel>(
+      model: UserModel(),
+      child: MaterialApp(
+        title: "Flutter's Clothing",
+        theme: ThemeData(
+            primarySwatch: Colors.blue,
+            primaryColor: Color.fromARGB(255, 4, 125, 141)
+        ),
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen(),
       ),
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
     );
   }
 }
