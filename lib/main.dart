02ea45760d50c953e5c3a51f146f687699dbaa7f
arthur.nunes <arthur.nunes@dtidigital.com.br>
2021-05-21 23:31:48 -0300

    added scopedModelDescendant in dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:loja_virtualflutter/models/cart_model.dart';
 import 'package:loja_virtualflutter/models/user_model.dart';
 import 'package:loja_virtualflutter/screens/home_screen.dart';
 import 'package:scoped_model/scoped_model.dart';
@@ -16,15 +17,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScopedModel<UserModel>(
       model: UserModel(),
-      child: MaterialApp(
-        title: "Flutter's Clothing",
-        theme: ThemeData(
-            primarySwatch: Colors.blue,
-            primaryColor: Color.fromARGB(255, 4, 125, 141)
-        ),
-        debugShowCheckedModeBanner: false,
-        home: HomeScreen(),
-      ),
+      child: ScopedModelDescendant<UserModel>(
+        builder: (context,child,model){
+          return ScopedModel<CartModel>(
+            model: CartModel(model),
+            child: MaterialApp(
+              title: "Flutter's Clothing",
+              theme: ThemeData(
+                  primarySwatch: Colors.blue,
+                  primaryColor: Color.fromARGB(255, 4, 125, 141)
+              ),
+              debugShowCheckedModeBanner: false,
+              home: HomeScreen(),
+            ),
+          );
+        },
+      )
     );
   }
 }
