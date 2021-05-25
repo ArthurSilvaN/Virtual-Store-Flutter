7f23f6481163c9a2516d608a855c9225b3ccd945
arthur.nunes <arthur.nunes@dtidigital.com.br>
2021-05-25 16:35:04 -0300

    update product screen

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -39,7 +39,7 @@ class _ProductScreenState extends State<ProductScreen> {
           AspectRatio(
             aspectRatio: 0.9,
             child: Carousel(
-              images: product.images.map((url){
+              images: product.images.map((url) {
                 return NetworkImage(url);
               }).toList(),
               dotSize: 4.0,
@@ -50,123 +50,119 @@ class _ProductScreenState extends State<ProductScreen> {
             ),
           ),
           Padding(
-           padding: EdgeInsets.all(4.0),
-           child: Column(
-             crossAxisAlignment: CrossAxisAlignment.stretch,
-             children: [
-               Text(
-                 product.title,
-                 style: TextStyle(
-                   fontSize: 20.0,
-                   fontWeight: FontWeight.w400
-                 ),
-                 maxLines: 3,
-               ),
-               Text(
-                 "R\$ ${product.price.toStringAsFixed(2)}",
-                 style: TextStyle(
-                   fontSize: 22.0,
-                   fontWeight: FontWeight.bold,
-                   color: primaryColor
-                 ),
-               ),
-               SizedBox(height: 16,),
-               Text(
-                 "Tamanho",
-                 style: TextStyle(
-                   fontSize: 18.0,
-                   fontWeight: FontWeight.w400
-                 ),
-               ),
-               SizedBox(
-                 height: 34.0,
-                 child: GridView(
-                   padding: EdgeInsets.symmetric(vertical: 4.0),
-                   scrollDirection: Axis.horizontal,
-                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                     crossAxisCount: 1,
-                     mainAxisSpacing: 8.0,
-                     childAspectRatio: 0.5
-                   ),
-                   children: product.sizes.map((e){
-                     return GestureDetector(
-                       onTap: (){
-                         setState(() {
-                           size = e;
-                         });
-                       },
-                       child: Container(
-                         decoration: BoxDecoration(
-                           borderRadius: BorderRadius.all(Radius.circular(4.0)),
-                           border: Border.all(
-                             color: e == size ? primaryColor : Colors.grey[500],
-                             width: 3.0
-                           )
-                         ),
-                         width: 50.0,
-                         alignment: Alignment.center,
-                         child: Text(e),
-                       ),
-                     );
-                   },
-                   ).toList(),
-                 ),
-               ),
-               SizedBox(height: 16.0,),
-               SizedBox(
-                 height: 44.0,
-                 // ignore: deprecated_member_use
-                 child: RaisedButton(
-                   onPressed: size != null ?
-                   (){
-                     if(UserModel.of(context).isLoggedIn()){
-                       CartProduct cartProduct = CartProduct();
-                       cartProduct.size = size;
-                       cartProduct.quantity = 1;
-                       cartProduct.pid = product.id;
-                       cartProduct.category = product.category;
+            padding: EdgeInsets.all(4.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  product.title,
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w400),
+                  maxLines: 3,
+                ),
+                Text(
+                  "R\$ ${product.price.toStringAsFixed(2)}",
+                  style: TextStyle(
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.bold,
+                      color: primaryColor),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  "Tamanho",
+                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w400),
+                ),
+                SizedBox(
+                  height: 34.0,
+                  child: GridView(
+                    padding: EdgeInsets.symmetric(vertical: 4.0),
+                    scrollDirection: Axis.horizontal,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 1,
+                        mainAxisSpacing: 8.0,
+                        childAspectRatio: 0.5),
+                    children: product.sizes.map(
+                      (e) {
+                        return GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              size = e;
+                            });
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(4.0)),
+                                border: Border.all(
+                                    color: e == size
+                                        ? primaryColor
+                                        : Colors.grey[500],
+                                    width: 3.0)),
+                            width: 50.0,
+                            alignment: Alignment.center,
+                            child: Text(e),
+                          ),
+                        );
+                      },
+                    ).toList(),
+                  ),
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                SizedBox(
+                  height: 44.0,
+                  // ignore: deprecated_member_use
+                  child: RaisedButton(
+                    onPressed: size != null
+                        ? () {
+                            if (UserModel.of(context).isLoggedIn()) {
+                              CartProduct cartProduct = CartProduct();
+                              cartProduct.size = size;
+                              cartProduct.quantity = 1;
+                              cartProduct.pid = product.id;
+                              cartProduct.category = product.category;
 
-                       CartModel.of(context).addCartItem(cartProduct);
+                              CartModel.of(context).addCartItem(cartProduct);
 
-                       Navigator.of(context).push(
-                           MaterialPageRoute(builder: (context) => CartScreen()
-                           )
-                       );
-
-                     }else{
-                       Navigator.of(context).push(
-                         MaterialPageRoute(builder: (context) => LoginScreen())
-                       );
-                     }
-                   } : null,
-                   child: Text(
-                     UserModel.of(context).isLoggedIn() ? "Adicionar ao Carrinho" : "Entre para Comprar",
-                     style: TextStyle(fontSize: 18.0),
-                   ),
-                   color: primaryColor,
-                   textColor: Colors.white,
-                 ),
-               ),
-               SizedBox(height: 16.0,),
-               Text(
-                 "Descrição",
-                 style: TextStyle(
-                   fontSize: 16.0,
-                   fontWeight: FontWeight.w400,
-                 ),
-               ),
-               Text(
-                 product.description,
-                 style: TextStyle(
-                   fontSize: 16.0
-                 ),
-               )
-             ],
-           ),
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => CartScreen()));
+                            } else {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => LoginScreen()));
+                            }
+                          }
+                        : null,
+                    child: Text(
+                      UserModel.of(context).isLoggedIn()
+                          ? "Adicionar ao Carrinho"
+                          : "Entre para Comprar",
+                      style: TextStyle(fontSize: 18.0),
+                    ),
+                    color: primaryColor,
+                    textColor: Colors.white,
+                  ),
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                Text(
+                  "Descrição",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                Text(
+                  product.description,
+                  style: TextStyle(fontSize: 16.0),
+                )
+              ],
+            ),
           )
         ],
       ),
     );
   }
 }
-
