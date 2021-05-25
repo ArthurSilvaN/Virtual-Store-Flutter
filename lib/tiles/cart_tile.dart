4772340bede61f3997b86fec40a3e08db820b4f2
arthur.nunes <arthur.nunes@dtidigital.com.br>
2021-05-24 21:09:35 -0300

    create cart tile

---
+++ lib/tiles/cart_tile.dart
@@ -0,0 +1,106 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:loja_virtualflutter/data/cart_product_data.dart';
+import 'package:loja_virtualflutter/data/product_data.dart';
+import 'package:loja_virtualflutter/models/cart_model.dart';
+
+class CartTile extends StatelessWidget {
+  final CartProduct cartProduct;
+
+  CartTile(this.cartProduct);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget _buildContent() {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.all(8.0),
+            width: 120.0,
+            child: Image.network(
+              cartProduct.productData.images[0],
+              fit: BoxFit.cover,
+            ),
+          ),
+          Expanded(
+              child: Container(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  cartProduct.productData.title,
+                  style: TextStyle(fontWeight: FontWeight.w400, fontSize: 17.0),
+                ),
+                Text(
+                  "R\$  ${cartProduct.productData.price.toStringAsFixed(2)}",
+                  style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.remove),
+                      color: Theme.of(context).primaryColor,
+                      onPressed: cartProduct.quantity > 1
+                          ? () {
+                              CartModel.of(context).decProduct(cartProduct);
+                            }
+                          : null,
+                    ),
+                    Text(cartProduct.quantity.toString()),
+                    IconButton(
+                      onPressed: () {
+                        CartModel.of(context).incProduct(cartProduct);
+                      },
+                      icon: Icon(Icons.add),
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    FlatButton(
+                      child: Text("Remover"),
+                      textColor: Colors.grey[500],
+                      onPressed: () {
+                        CartModel.of(context).removeCartItem(cartProduct);
+                      },
+                    )
+                  ],
+                )
+              ],
+            ),
+          ))
+        ],
+      );
+    }
+
+    return Card(
+        margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+        child: cartProduct.productData == null
+            ? FutureBuilder<DocumentSnapshot>(
+                future: FirebaseFirestore.instance
+                    .collection("products")
+                    .doc(cartProduct.category)
+                    .collection("intems")
+                    .doc(cartProduct.pid)
+                    .get(),
+                // ignore: missing_return
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    cartProduct.productData =
+                        ProductData.fromDocument(snapshot.data);
+                  } else {
+                    return Container(
+                      height: 70.0,
+                      child: CircularProgressIndicator(),
+                      alignment: Alignment.center,
+                    );
+                  }
+                },
+              )
+            : _buildContent());
+  }
+}
