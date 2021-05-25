0b60f549e7943e3dd438dae8a141c84ff8571cb1
arthur.nunes <arthur.nunes@dtidigital.com.br>
2021-05-25 16:34:30 -0300

    add partners in home tab

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,30 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:loja_virtualflutter/Tabs/home_tab.dart';
+import 'package:loja_virtualflutter/tabs/orders_tab.dart';
+import 'package:loja_virtualflutter/tabs/partners_tab.dart';
 import 'package:loja_virtualflutter/tabs/products_tab.dart';
 import 'package:loja_virtualflutter/widgets/cart_button.dart';
 import 'package:loja_virtualflutter/widgets/custom_drawer.dart';
 
 class HomeScreen extends  StatelessWidget {
-  final _pageControler = PageController();
+  final _pageController = PageController();
 
   @override
   Widget build(BuildContext context) {
     return PageView(
-      controller: _pageControler,
+      controller: _pageController,
       physics: NeverScrollableScrollPhysics(),
       children: <Widget>[
         Scaffold(
           floatingActionButton: CartButton(),
           body: HomeTab(),
-          drawer: CustomDrawer(_pageControler),
+          drawer: CustomDrawer(_pageController),
         ),
         Scaffold(
           appBar: AppBar(
             title: Text("Produtos"),
             centerTitle: true,
           ),
-          drawer: CustomDrawer(_pageControler),
+          drawer: CustomDrawer(_pageController),
           body: ProductsTab(),
+          floatingActionButton: CartButton(),
+        ),
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Parceiros"),
+            centerTitle: true,
+          ),
+          body: PartnersTab(),
+          drawer: CustomDrawer(_pageController),
+        ),
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Meus Pedidos"),
+            centerTitle: true,
+          ),
+          body: OrdersTab(),
+          drawer: CustomDrawer(_pageController),
         )
       ],
     );
