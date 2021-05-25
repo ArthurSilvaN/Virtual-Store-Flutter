37f97e1ef350146578d1b136f780203454ac2018
arthur.nunes <arthur.nunes@dtidigital.com.br>
2021-05-25 16:37:45 -0300

    crated partners tile

---
+++ lib/tiles/partners_tile.dart
@@ -0,0 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+class PartnersTile extends StatelessWidget {
+
+  final DocumentSnapshot snapshot;
+
+  PartnersTile(this.snapshot);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card();
+  }
+}
