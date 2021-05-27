fc88dc9df1cff59b191ee7c22dd06141a6144798
arthur.nunes <arthur.nunes@dtidigital.com.br>
2021-05-26 21:35:19 -0300

    updated product data

--- lib/data/product_data.dart
+++ lib/data/product_data.dart
@@ -13,14 +13,14 @@ class ProductData {
     id = snapshot.id;
     title = snapshot.data()["title"];
     description = snapshot.data()["description"];
-    price = snapshot.data()["price"];
+    price = snapshot.data()["price"] + 0.0;
     images = snapshot.data()["images"];
     sizes = snapshot.data()["sizes"];
   }
+
   Map<String, dynamic> toResumeMap(){
     return{
       "title": title,
-      "description": description,
       "price": price
     };
   }
